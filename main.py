987d2bab8d6ed9ca8032be3f0b370612fa291b3b
mshvdkmv <evmiev@icloud.com>
2019-04-21 22:46:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import requests, json
+import pickle
+TOKEN = 'oK0jD065025D3iN4NZFkw64w171DBYy9HJOe0BFDC-huWjb229LOOsXCHe2AuHgo'
+BASE_URL = 'https://api.genius.com'
+
+
+def get_song_info(song_id):
+    headers = {'Authorization': 'Bearer ' + TOKEN}
+    search_url = BASE_URL + '/songs/' + str(song_id)
+    response = requests.get(search_url, headers=headers).json()
+
+    return response
+
+
+def get_artist_and_song_url(song_id):
+    status = -5
+    name, link, release_date = '', '', ''
+    info = get_song_info(song_id)
+    status = info['meta']['status']
+    print(song_id)
+    if status == 200:
+        link = info['response']['song']['path']
+        name = info['response']['song']['primary_artist']['name']
+        release_date = info['response']['song']['release_date']
+    return status, name, link, release_date
+
+
+def build_artist_dict(start, end):
+    artist_dict = dict()
+    while start <= end:
+        status, name, link, date = get_artist_and_song_url(start)
+        if status == 200:
+            if name in artist_dict.keys():
+                artist_dict[name].append((link, date))
+            else:
+                artist_dict[name] = list()
+                artist_dict[name].append((link, date))
+        start += 1
+    return artist_dict
+
+
+def main():
+    ad = build_artist_dict(1, 28000)
+    with open('results.p', 'wb') as f:
+        pickle.dump(ad, f)
+
+
+if __name__ == "__main__":
+    main()
+
