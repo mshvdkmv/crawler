3617dd5f2fba05470da83646a827e1e984eb3789
mshvdkmv <evmiev@icloud.com>
2019-04-22 23:55:09 +0300

    add recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests, json
 import pickle
-TOKEN = 'ujcJqpQBid1pt6rJDkPKQHMMn5boncVwUTgBGh2DfPUi_MXs824FG9y72YRcr8Km'
+TOKEN = 'oZYuZ4iWgymVsXYGj1xx8DXLZQ5fXrbx8UN__wwZODtLq0hf37svS6d1wWUoStQi'
 BASE_URL = 'https://api.genius.com'
 
 
@@ -66,7 +66,7 @@ def save(name, data):
 
 
 def main():
-    ad, index = build_artist_dict(150000, 200000)
+    ad, index = build_artist_dict(0, 56000)
     filename = str(index) + '.p'
     with open(filename, 'wb') as f:
         pickle.dump(ad, f)
